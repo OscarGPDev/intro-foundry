05c940847c4b6df912fde6fd3026cede06065ffc
Oscar GP <oscarprograb@gmail.com>
2025-08-16 13:56:15 -0600

    init project with foundry

---
+++ src/HolaMundo.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.8.13;
+contract HolaMundo {
+    string private mensaje;
+    constructor() {
+        mensaje = "Hola Mundo";
+    }
+    function obtenerMensaje() public view returns (string memory) {
+        return mensaje;
+    }
+    function actualizarMensaje(string calldata nuevoMensaje) public {
+        mensaje = nuevoMensaje;
+    }
+}
---
+++ test/HolaMundo.t.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.8.13;
+import "forge-std/Test.sol";
+import "../src/HolaMundo.sol";
+contract HolaMundoTest is Test {
+    HolaMundo public holaMundo;
+    function setUp() public {
+        holaMundo = new HolaMundo();
+    }
+    function test_ObtenerMensaje() public view {
+        assertEq(holaMundo.obtenerMensaje(), "Hola Mundo");
+    }
+    function test_ActualizarMensaje() public {
+        string memory nuevoMensaje = "mensaje de prueba";
+        holaMundo.actualizarMensaje(nuevoMensaje);
+        assertEq(holaMundo.obtenerMensaje(), nuevoMensaje);
+    }
+}
