4a9c777063d350edd8f13ba67681ceb610f98018
Oscar GP <oscarprograb@gmail.com>
2025-08-30 14:18:08 -0600

    feat: DoS vulnerability example(fmt formatted)

--- src/vulnerabilities/DoS.sol
+++ src/vulnerabilities/DoS.sol
@@ -3,20 +3,22 @@ pragma solidity >=0.8.0 <0.9.0;
 
 contract DoS {
     // This is is not only a vulnerability of costly operation but also could lead to DoS attacks
-    function unrestrictedConsumption(uint _operations) public pure {
-        uint[] memory data = new uint[](_operations);
-        for (uint i = 0; i < _operations; i++) {
+    function unrestrictedConsumption(uint256 _operations) public pure {
+        uint256[] memory data = new uint256[](_operations);
+        for (uint256 i = 0; i < _operations; i++) {
             data[i] = i;
         }
     }
 }
+
 contract TrES {
-    uint constant REASONABLE_LIMIT = 1000;
-    // This way we avoid both 
-    function unrestrictedConsumption(uint _operations) public pure {
+    uint256 constant REASONABLE_LIMIT = 1000;
+    // This way we avoid both
+
+    function unrestrictedConsumption(uint256 _operations) public pure {
         require(_operations <= REASONABLE_LIMIT, "Too many operations");
-        uint[] memory data = new uint[](_operations);
-        for (uint i = 0; i < _operations; i++) {
+        uint256[] memory data = new uint256[](_operations);
+        for (uint256 i = 0; i < _operations; i++) {
             data[i] = i;
         }
     }
